58694438d1ea0cb6ab53d38ba466f053f9cf0ba1
BEE2V <135147675+BEE2V@users.noreply.github.com>
2026-07-14 15:14:57 +0530

    Display Test 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,47 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
 
-#define ENCODER_CLK 20
-#define ENCODER_DT 21
+// TFT Pins
+#define TFT_CS 10
+#define TFT_DC 8
+#define TFT_RST 9
 
-volatile int encoderCount = 0;
-volatile int lastCLK = HIGH;
+#define TFT_MOSI 11
+#define TFT_SCLK 12
+
+Adafruit_ST7789 tft = Adafruit_ST7789(
+    TFT_CS,
+    TFT_DC,
+    TFT_RST);
 
 void setup()
 {
   Serial.begin(115200);
 
-  pinMode(ENCODER_CLK, INPUT_PULLUP);
-  pinMode(ENCODER_DT, INPUT_PULLUP);
+  SPI.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
 
-  lastCLK = digitalRead(ENCODER_CLK);
+  tft.init(240, 320);
+  tft.setRotation(0);
 
-  Serial.println("Encoder Test");
+  tft.fillScreen(ST77XX_BLACK);
+
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(2);
+
+  tft.setCursor(20, 20);
+  tft.println("ESP32-S3");
+
+  tft.setCursor(20, 50);
+  tft.println("GMT024-08");
+
+  tft.drawRect(10, 100, 100, 60, ST77XX_RED);
+  tft.fillCircle(180, 130, 30, ST77XX_BLUE);
+
+  Serial.println("Display Test OK");
 }
 
 void loop()
 {
-  int currentCLK = digitalRead(ENCODER_CLK);
-
-  if (currentCLK != lastCLK && currentCLK == LOW)
-  {
-    if (digitalRead(ENCODER_DT) != currentCLK)
-    {
-      encoderCount++;
-    }
-    else
-    {
-      encoderCount--;
-    }
-
-    Serial.print("Count: ");
-    Serial.println(encoderCount);
-  }
-
-  lastCLK = currentCLK;
 }
