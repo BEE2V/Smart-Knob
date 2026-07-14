56cb0547e33500694ca078a6d0e0faa8b7229983
BEE2V <135147675+BEE2V@users.noreply.github.com>
2026-07-14 22:45:40 +0530

    Display with Encoder and Buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,93 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7789.h>
 
-// Encoder
+// ================= TFT =================
+#define TFT_CS 10
+#define TFT_DC 8
+#define TFT_RST 9
+
+#define TFT_MOSI 11
+#define TFT_SCLK 12
+
+Adafruit_ST7789 tft(TFT_CS, TFT_DC, TFT_RST);
+
+// ================= Inputs =================
 #define ENC_CLK 20
 #define ENC_DT 21
 #define ENC_SW 47
 
-// Buttons
 #define BTN1 48
 #define BTN2 45
 #define BTN3 0
 #define BTN4 35
 
+// ================= Encoder =================
 int encoderCount = 0;
-int lastCLK;
+int encoderSteps = 0;
+
+int lastEncoded = 0;
+unsigned long lastEncoderTime = 0;
+
+// ================= Circle Positions =================
+
+#define R 20
+
+struct ButtonState
+{
+  int x;
+  int y;
+  bool last;
+};
+
+ButtonState buttons[5] =
+    {
+        {60, 150, false},  // BTN1
+        {180, 150, false}, // BTN2
+        {60, 270, false},  // BTN3
+        {180, 270, false}, // BTN4
+        {120, 210, false}  // SW
+};
+
+// ================= Drawing =================
+
+void drawCircle(ButtonState &b, bool pressed)
+{
+  if (pressed)
+  {
+    tft.fillCircle(b.x, b.y, R, ST77XX_GREEN);
+  }
+  else
+  {
+    tft.fillCircle(b.x, b.y, R, ST77XX_BLACK);
+    tft.drawCircle(b.x, b.y, R, ST77XX_WHITE);
+  }
+}
+
+void updateEncoderDisplay()
+{
+  tft.fillRect(15, 15, 210, 50, ST77XX_BLACK);
+
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(3);
+  tft.setCursor(25, 25);
+
+  tft.print("Count:");
+  tft.print(encoderCount);
+}
+
+// ================= Setup =================
 
 void setup()
 {
   Serial.begin(115200);
+
+  SPI.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
+
+  tft.init(240, 320);
+  tft.setRotation(0);
+  tft.fillScreen(ST77XX_BLACK);
 
   pinMode(ENC_CLK, INPUT_PULLUP);
   pinMode(ENC_DT, INPUT_PULLUP);
@@ -27,60 +98,88 @@ void setup()
   pinMode(BTN3, INPUT_PULLUP);
   pinMode(BTN4, INPUT_PULLUP);
 
-  lastCLK = digitalRead(ENC_CLK);
+  // Initial encoder state
+  lastEncoded =
+      (digitalRead(ENC_CLK) << 1) | digitalRead(ENC_DT);
 
-  Serial.println("Smart Home Input Test");
+  tft.drawRect(10, 10, 220, 60, ST77XX_WHITE);
+
+  for (int i = 0; i < 5; i++)
+    drawCircle(buttons[i], false);
+
+  updateEncoderDisplay();
 }
+
+// ================= Loop =================
 
 void loop()
 {
 
-  // Encoder
-  int clk = digitalRead(ENC_CLK);
+  // -------- Encoder --------
 
-  if (clk != lastCLK)
+  int MSB = digitalRead(ENC_CLK);
+  int LSB = digitalRead(ENC_DT);
+
+  int encoded = (MSB << 1) | LSB;
+
+  int sum = (lastEncoded << 2) | encoded;
+
+  int movement = 0;
+
+  // clockwise
+  if (sum == 0b1101 ||
+      sum == 0b0100 ||
+      sum == 0b0010 ||
+      sum == 0b1011)
   {
-
-    if (clk == digitalRead(ENC_DT))
-      encoderCount--;
-    else
-      encoderCount++;
-
-    Serial.print("Encoder: ");
-    Serial.println(encoderCount);
+    movement = 1;
   }
 
-  lastCLK = clk;
-
-  // Encoder button
-  if (!digitalRead(ENC_SW))
+  // counter clockwise
+  if (sum == 0b1110 ||
+      sum == 0b0111 ||
+      sum == 0b0001 ||
+      sum == 0b1000)
   {
-    Serial.println("Encoder SW");
-    delay(200);
+    movement = -1;
   }
 
-  // Buttons
-  if (!digitalRead(BTN1))
+  if (movement != 0)
   {
-    Serial.println("BTN1");
-    delay(200);
+    encoderSteps += movement;
+
+    // One physical click = one count
+    if (abs(encoderSteps) >= 2)
+    {
+      if (encoderSteps > 0)
+        encoderCount++;
+      else
+        encoderCount--;
+
+      encoderSteps = 0;
+
+      updateEncoderDisplay();
+    }
   }
 
-  if (!digitalRead(BTN2))
-  {
-    Serial.println("BTN2");
-    delay(200);
-  }
+  lastEncoded = encoded;
 
-  if (!digitalRead(BTN3))
-  {
-    Serial.println("BTN3");
-    delay(200);
-  }
+  // -------- Buttons --------
 
-  if (!digitalRead(BTN4))
+  bool states[5];
+
+  states[0] = !digitalRead(BTN1);
+  states[1] = !digitalRead(BTN2);
+  states[2] = !digitalRead(BTN3);
+  states[3] = !digitalRead(BTN4);
+  states[4] = !digitalRead(ENC_SW);
+
+  for (int i = 0; i < 5; i++)
   {
-    Serial.println("BTN4");
-    delay(200);
+    if (states[i] != buttons[i].last)
+    {
+      drawCircle(buttons[i], states[i]);
+      buttons[i].last = states[i];
+    }
   }
 }
