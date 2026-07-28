4fa546adb4e89dc6568382cd383a6d0692495969
BEE2V <135147675+BEE2V@users.noreply.github.com>
2026-07-28 16:21:00 +0530

    Firmware Update for OTA Update issue

--- src/secrets.example.h
+++ src/secrets.example.h
@@ -7,5 +7,5 @@ constexpr const char *MQTT_USERNAME = "smartknob_mqtt";
 constexpr const char *MQTT_PASSWORD = "replace_with_your_password";
 
 // Use a different password from Wi-Fi and MQTT.
-constexpr const char *OTA_HOST = "192.168.8.105";
+constexpr const char *OTA_HOST = "192.168.8.101";
 constexpr const char *OTA_PASSWORD = "replace_with_a_strong_ota_password";
